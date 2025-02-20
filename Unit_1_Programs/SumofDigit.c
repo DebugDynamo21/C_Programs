512d8788a243d328af9c1b78883f5aeb305c2218
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-02-20 12:49:45 +0530

    Sum of digit

---
+++ Unit_1_Programs/SumofDigit.c
@@ -0,0 +1,36 @@
+/* If a five digit number is input through the keyboard, write a
+program to obtain the sum of the first and last digit of this
+number. */
+
+#include <stdio.h>
+int main(){
+    long int n;
+    int a, sum = 0;
+    printf("Enter a 5 digit number: ");
+    scanf("%ld", &n);   //input a 5 digit number
+
+    //Logic to find sum of digit of a 5 digit number
+
+    a = n % 10;
+    sum = sum + a;
+    n = n / 10;
+
+    a = n % 10;
+    sum = sum + a;
+    n = n / 10;
+
+    a = n % 10;
+    sum = sum + a;
+    n = n / 10;
+
+    a = n % 10;
+    sum = sum + a;
+    n = n / 10;
+
+    a = n % 10;
+    sum = sum + a;
+    n = n / 10;
+
+    printf("\nThe sum of digit is: %d\n", sum);     //print the sum of digit
+    return 0; 
+}
