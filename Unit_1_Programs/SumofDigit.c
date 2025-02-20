ae609e2c56031359cb507e598d91f49f952890a1
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-02-20 12:51:06 +0530

    Sum of digit

--- Unit_1_Programs/SumofDigit.c
+++ Unit_1_Programs/SumofDigit.c
@@ -1,6 +1,5 @@
 /* If a five digit number is input through the keyboard, write a
-program to obtain the sum of the first and last digit of this
-number. */
+program to obtain the sum of the digit of this number. */
 
 #include <stdio.h>
 int main(){
